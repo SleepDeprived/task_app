690e729d133c4233d8298b7c890acbba2464d8d8
Tom Metzger <tom@tmetzger.com>
2014-09-07 16:46:39 -0400

    added mailer parameters info to controller and created the email template for the mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "metzgert@gmail.com"
+
+  def welcome_email(user)
+    @user = user
+    @url = "http://www.google.com" # this needs to be changed to the application url for task app that this will redirect to that confirms a user's email (preferably a post with a redirect)
+    mail(to: @user.email, subject:"Welcome to Task App")
+  end
 end
