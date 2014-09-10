0468a27baf91d9fe7d9dc7cd9ba284e3d9fea627
Tom Metzger <tom@tmetzger.com>
2014-09-09 22:22:23 -0400

    ran migration associated with task model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140830015958) do
+ActiveRecord::Schema.define(version: 20140910022122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: true do |t|
+    t.string   "name"
+    t.integer  "priority_id"
+    t.string   "type"
+    t.integer  "project_id"
+    t.integer  "budgeted_time"
+    t.integer  "total_time"
+    t.string   "task_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
