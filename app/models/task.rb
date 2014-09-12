c8c9c40a54b5bc519dee3756e81c48eaa7029def
Tom Metzger <tom@tmetzger.com>
2014-09-11 22:18:33 -0400

    added validation for task name

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,11 @@
 class Task < ActiveRecord::Base
 
-params.permit(:name, :priority_id, :type, :project_id, :budgeted_tiem, :total_time, :task_type)
+# params.permit(:name, :priority_id, :type, :project_id, :budgeted_tiem, :total_time, :task_type)
+
+validates_presence_of :name
+
+# belongs_to :priority
+
 
 
 end
