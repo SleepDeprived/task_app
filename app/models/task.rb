c7823a6f64b60ad14693b9b45b5bf4a523393c28
Tom Metzger <tom@tmetzger.com>
2014-09-11 23:09:03 -0400

    created methods to convert budgeted time from seconds into a string describing the task duration

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,6 +6,30 @@ validates_presence_of :name
 
 # belongs_to :priority
 
+  def budgeted_time_output(time_in_seconds)
+    hours = calculate_hours(time_in_seconds)
+    minutes = calculate_minutes(time_in_seconds)
+    concat_strings = hours > 0 && minutes > 0 ? " and " : ""
+    time_unit_as_string(hours, "hour") + concat_strings + time_unit_as_string(minutes, "minute")
 
+  end
+
+  def time_unit_as_string(time_quantity, singular_time_unit)
+    if time_quantity == 1
+      "#{time_quantity} #{singular_time_unit}"
+    elsif time_quantity > 1
+      "#{time_quantity} #{singular_time_unit}s"
+    else
+      ""
+    end
+  end
+
+  def calculate_hours(time_in_seconds)
+    time_in_seconds / (60 * 60)
+  end
+
+  def calculate_minutes(time_in_seconds)
+    (time_in_seconds / 60) % 60
+  end
 
 end
